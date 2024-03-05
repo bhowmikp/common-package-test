968d964fd26ae9eb471ce29b5e194d31e184918d
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2024-03-04 20:56:53 -0500

    tset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
+// This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  ignorePatterns: ["apps/**", "packages/**"],
+  extends: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
